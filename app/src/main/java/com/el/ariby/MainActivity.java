ab07408bc5ccfa533e8b8d5215250260529aa7cd
takedawon <takedawon@naver.com>
2019-01-29 16:19:58 +0900

    메인 액티비티 추가

--- app/src/main/java/com/el/ariby/MainActivity.java
+++ app/src/main/java/com/el/ariby/MainActivity.java
@@ -1,13 +1,30 @@
 package com.el.ariby;
 
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
-
+    VideoView videoV;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN); // 풀스크린으로 세팅(상단바, 메뉴바 없어짐)
         setContentView(R.layout.activity_main);
+        videoV=findViewById(R.id.videoView);
+        Uri video = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.mainvideo);//비디오 파일 경로 설정
+        videoV.setVideoURI(video);
+
+        videoV.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                videoV.start(); // 비디오 무한 반복
+            }
+        });
+        videoV.start();
     }
 }
