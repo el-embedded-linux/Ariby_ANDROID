afc36d9e3215e3e993eea8fb4e147d46c3e6cbde
takedawon <takedawon@naver.com>
2019-05-12 18:09:17 +0900

    길찾기 경로 라인으로 그려주는 기능 추가

--- app/src/main/java/com/el/ariby/ui/main/menu/navigation/MapFindLocationActivity.java
+++ app/src/main/java/com/el/ariby/ui/main/menu/navigation/MapFindLocationActivity.java
@@ -1,6 +1,7 @@
 package com.el.ariby.ui.main.menu.navigation;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.graphics.Point;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -14,11 +15,15 @@ import com.el.ariby.ui.api.MapFindApi;
 import com.el.ariby.ui.api.SelfCall;
 import com.el.ariby.ui.api.response.MapFindRepoResponse;
 
+import net.daum.mf.map.api.CameraUpdateFactory;
 import net.daum.mf.map.api.MapPOIItem;
 import net.daum.mf.map.api.MapPoint;
+import net.daum.mf.map.api.MapPointBounds;
+import net.daum.mf.map.api.MapPolyline;
 import net.daum.mf.map.api.MapView;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -55,6 +60,7 @@ public class MapFindLocationActivity extends AppCompatActivity {
         MapPoint markerPointEnd = MapPoint.mapPointWithGeoCoord(
                 Double.parseDouble(endX), Double.parseDouble(endY));
 
+
         mapView.setMapCenterPoint(markerPointStart, true);
         MapPOIItem marker = new MapPOIItem(); // 마커 생성
         marker.setItemName("Dafault Market");
@@ -70,7 +76,7 @@ public class MapFindLocationActivity extends AppCompatActivity {
         marker2.setMarkerType(MapPOIItem.MarkerType.BluePin);
         marker2.setSelectedMarkerType(MapPOIItem.MarkerType.RedPin);
 
-        ViewGroup mapViewContainer = (ViewGroup) findViewById(R.id.map_view);
+        final ViewGroup mapViewContainer = (ViewGroup) findViewById(R.id.map_view);
         mapViewContainer.addView(mapView);
         mapView.addPOIItem(marker);
         mapView.addPOIItem(marker2);
@@ -80,6 +86,8 @@ public class MapFindLocationActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if (mapView.getCurrentLocationTrackingMode() == MapView.CurrentLocationTrackingMode.TrackingModeOff) {
+                    mapView.setZoomLevel(-1, true);
+                    mapView.zoomIn(true);
                     mapView.setCurrentLocationTrackingMode(MapView.CurrentLocationTrackingMode.TrackingModeOnWithoutHeading);
                 } else if (mapView.getCurrentLocationTrackingMode() == MapView.CurrentLocationTrackingMode.TrackingModeOnWithoutHeading) {
                     mapView.setCurrentLocationTrackingMode(MapView.CurrentLocationTrackingMode.TrackingModeOnWithHeading);
@@ -90,7 +98,7 @@ public class MapFindLocationActivity extends AppCompatActivity {
         });
     }
 
-    private void getMapFind(String startX, String startY, String endX, String endY) {
+    private void getMapFind(final String startX, final String startY, final String endX, final String endY) {
         Retrofit retrofit = SelfCall.createRetrofit(MapFindApi.BASEURL);
         MapFindApi apiService = retrofit.create(MapFindApi.class);
         Call<MapFindRepoResponse> call =
@@ -105,27 +113,49 @@ public class MapFindLocationActivity extends AppCompatActivity {
                 MapFindRepoResponse repo = response.body();
                 int featuresSize = repo.getFeatures().size();
 
+                MapPolyline polyline = new MapPolyline();
+                polyline.setTag(1000);
+                polyline.setLineColor(Color.argb(128, 255, 51, 0)); // Polyline 컬러 지정.
+
+                polyline.addPoint(MapPoint.mapPointWithGeoCoord(Double.parseDouble(startY), Double.parseDouble(startX)));
                 for (int i = 0; i < featuresSize; i++) {
                     String type = repo.getFeatures().get(i).getGeometry().getType();
+                    Double x;
+                    Double y;
                     if (type.equals("Point")) {
-                        Double x = (Double) repo.getFeatures().get(i).getGeometry().getCoordinates().get(0);
-                        Double y = (Double) repo.getFeatures().get(i).getGeometry().getCoordinates().get(1);
+                        x = (Double) repo.getFeatures().get(i).getGeometry().getCoordinates().get(0);
+                        y = (Double) repo.getFeatures().get(i).getGeometry().getCoordinates().get(1);
                         MapPoint marketPoint3 = MapPoint.mapPointWithGeoCoord(y, x);
 
                         MapPOIItem marker3 = new MapPOIItem(); // 마커 생성
                         marker3.setItemName("Dafault Market");
                         marker3.setTag(3);
                         marker3.setMapPoint(marketPoint3);
-                        marker3.setMarkerType(MapPOIItem.MarkerType.BluePin);
-                        marker3.setSelectedMarkerType(MapPOIItem.MarkerType.RedPin);
+                        marker3.setMarkerType(MapPOIItem.MarkerType.RedPin);
 
                         mapView.addPOIItem(marker3);
 
+                        polyline.addPoint(MapPoint.mapPointWithGeoCoord(y, x));
+
                         Log.d("testx" + i, ((Double) repo.getFeatures().get(i).getGeometry().getCoordinates().get(0)).toString());
-                        Log.d("testx" + i, ((Double) repo.getFeatures().get(i).getGeometry().getCoordinates().get(1)).toString());
+                        Log.d("testy" + i, ((Double) repo.getFeatures().get(i).getGeometry().getCoordinates().get(1)).toString());
+                    } else if (type.equals("LineString")) {
+                        List<Object> list = repo.getFeatures().get(i).getGeometry().getCoordinates();
+                        for (int k = 0; k < list.size(); k++) { // k
+                            String[] lit = String.valueOf(list.get(k)).split(" ");
+                            Double lineX = Double.parseDouble(lit[0].substring(1, lit[0].length()-1));
+                            Double lineY = Double.parseDouble(lit[1].substring(0, lit[1].length()-1));
+                            polyline.addPoint(MapPoint.mapPointWithGeoCoord(lineY, lineX));
+                        }
+                        Log.d("listtest", String.valueOf(list));
+                        Log.d("listtest", String.valueOf(list.size()));
                     }
                 }
-
+                polyline.addPoint(MapPoint.mapPointWithGeoCoord(Double.parseDouble(endY), Double.parseDouble(endX)));
+                mapView.addPolyline(polyline);
+                MapPointBounds mapPointBounds = new MapPointBounds(polyline.getMapPoints());
+                int padding = 100; // px
+                mapView.moveCamera(CameraUpdateFactory.newMapPointBounds(mapPointBounds, padding));
             }
 
             @Override
