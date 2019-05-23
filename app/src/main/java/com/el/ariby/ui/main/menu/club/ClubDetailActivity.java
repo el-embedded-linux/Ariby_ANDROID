302f6c78a301e6617eafca5d1f293ae3a25b9370
takedawon <takedawon@naver.com>
2019-05-23 20:58:25 +0900

    ClubDetailActivity 디자인 수정 및 FAB, AlertDialog 추가

--- app/src/main/java/com/el/ariby/ui/main/menu/club/ClubDetailActivity.java
+++ app/src/main/java/com/el/ariby/ui/main/menu/club/ClubDetailActivity.java
@@ -1,5 +1,6 @@
 package com.el.ariby.ui.main.menu.club;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
@@ -7,8 +8,10 @@ import android.os.Bundle;
 import android.view.View;
 
 import com.bumptech.glide.Glide;
+import com.el.ariby.MainActivity;
 import com.el.ariby.R;
 import com.el.ariby.databinding.ActivityClubDetailBinding;
+import com.el.ariby.ui.main.menu.ClubFragment;
 
 public class ClubDetailActivity extends AppCompatActivity {
     ActivityClubDetailBinding mBinding;
@@ -32,6 +35,20 @@ public class ClubDetailActivity extends AppCompatActivity {
             }
         });
 
+
+        mBinding.fabJoin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog alertDialog;
+                AlertDialog.Builder builder = new AlertDialog.Builder(v.getContext());
+                builder.setIcon(R.drawable.ic_account_circle_black_24dp);
+                builder.setTitle("클럽 가입");
+                builder.setMessage("가입하시겠습니까?");
+                builder.setPositiveButton("YES",null);
+                builder.setNegativeButton("NO",null);
+                builder.show();
+            }
+        });
 
     }
 }
