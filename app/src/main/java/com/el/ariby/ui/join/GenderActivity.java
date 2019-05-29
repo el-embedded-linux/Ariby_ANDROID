3856bb6e72b8f49dc5657bd419c15431c7f48195
takedawon <takedawon@naver.com>
2019-05-30 00:05:26 +0900

    GenderActivity 제대로 작동 안했던 버그 수정

--- app/src/main/java/com/el/ariby/ui/join/GenderActivity.java
+++ app/src/main/java/com/el/ariby/ui/join/GenderActivity.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import com.el.ariby.R;
 import com.el.ariby.databinding.ActivityGenderBinding;
@@ -14,13 +16,23 @@ import com.el.ariby.databinding.ActivityGenderBinding;
 public class GenderActivity extends AppCompatActivity {
     public final String PREFERENCE = "com.el.ariby_joining";
     ActivityGenderBinding mBinding;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN); // 풀스크린으로 세팅(상단바, 메뉴바 없어짐)
         mBinding = DataBindingUtil.setContentView(this, R.layout.activity_gender);
+
+        mBinding.grpGender.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if(checkedId == R.id.radio_male) {
+                    Toast.makeText(getApplicationContext(),"남자",Toast.LENGTH_SHORT).show();
+                } else if(checkedId == R.id.radio_female){
+                    Toast.makeText(getApplicationContext(),"여자",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
         mBinding.btnBack.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -33,11 +45,11 @@ public class GenderActivity extends AppCompatActivity {
         mBinding.btnNext.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(mBinding.radioMale.isSelected())
+                if(mBinding.radioMale.isChecked()) {
                     setPreference("gender", "M");
-                else
+                } else {
                     setPreference("gender", "F");
-
+                }
                 Intent intent = new Intent(getApplicationContext(), NicknameActivity.class);
                 startActivity(intent);
                 overridePendingTransition(R.anim.anim_slide_out_left, R.anim.anim_slide_in_right);
