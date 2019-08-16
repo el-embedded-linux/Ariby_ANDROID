f9cbdc9ff8b63209bbdef03d60f26c3917c06299
WG19 <mapofworld55@naver.com>
2019-08-17 01:07:32 +0900

    그룹라이딩_파이어베이스 구조 및 전체적인 코드 수정
    그룹원 간 상호 위치 확인 가능하도록 코드 수정

--- app/src/main/java/com/el/ariby/ui/main/menu/groupRiding/CreateGroupActivity.java
+++ app/src/main/java/com/el/ariby/ui/main/menu/groupRiding/CreateGroupActivity.java
@@ -21,6 +21,8 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.List;
+
 public class CreateGroupActivity extends AppCompatActivity {
     Button makeGroup;
     Button addFriend;
@@ -28,11 +30,6 @@ public class CreateGroupActivity extends AppCompatActivity {
     EditText inputStart;
     EditText inputEnd;
     EditText inputInfo;
-
-    String end_lat, end_lon;
-    String leader_nick;
-    String member_nick;
-    String uid;
 
     FirebaseDatabase database;
     DatabaseReference ref;
@@ -61,7 +58,90 @@ public class CreateGroupActivity extends AppCompatActivity {
         makeGroup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                UploadGroupInfoToFirebase();
+                //UploadGroupInfoToFirebase();
+                final FirebaseUser mUser = FirebaseAuth.getInstance().getCurrentUser();
+                userRef = database.getReference("USER").child(mUser.getUid());
+
+                SharedPreferences getList = getSharedPreferences("com.el.ariby_preferences", MODE_PRIVATE);
+                int count = getList.getInt("count", 0);
+                String str = getList.getString("members", "none");
+                Log.d("members : ", str);
+                Log.d("count : ", String.valueOf(count));
+                final String group_name = groupName.getText().toString();
+                String start = inputStart.getText().toString();
+                String end = inputEnd.getText().toString();
+                String note = inputInfo.getText().toString();
+
+                final String userInfo[] = new String[3];
+
+
+                String[] array = str.split("\\*");
+                //출발지
+                ref.child("GROUP_RIDING").child(group_name).child("startPoint").child("name").setValue(start);
+                ref.child("GROUP_RIDING").child(group_name).child("startPoint").child("lat").setValue(startY);
+                ref.child("GROUP_RIDING").child(group_name).child("startPoint").child("lon").setValue(startX);
+
+                //도착지
+                ref.child("GROUP_RIDING").child(group_name).child("endPoint").child("name").setValue(end);
+                ref.child("GROUP_RIDING").child(group_name).child("endPoint").child("lat").setValue(endY);
+                ref.child("GROUP_RIDING").child(group_name).child("endPoint").child("lon").setValue(endX);
+
+                //info
+                ref.child("GROUP_RIDING").child(group_name).child("info").child("note").setValue(note);
+
+
+
+                //생성자 닉네임
+                userRef.addListenerForSingleValueEvent(new ValueEventListener() {
+
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        //멤버 정보 (리더, 멤버)
+                        Log.d("userRef", "userRef works");
+
+                        userInfo[0]= mUser.getUid();
+                        userInfo[1]= dataSnapshot.child("nickname").getValue().toString();
+                        userInfo[2] = dataSnapshot.child("userImageURL").getValue().toString();
+
+                        Log.d("userRef str : ", String.valueOf(userInfo));
+
+                        ref.child("GROUP_RIDING").child(group_name).child("leader_nick").setValue(userInfo[1]);
+                        ref.child("GROUP_RIDING").child(group_name).child("members").child("0").child("uid").setValue(userInfo[0]);
+                        ref.child("GROUP_RIDING").child(group_name).child("members").child("0").child("nickname").setValue(userInfo[1]);
+                        ref.child("GROUP_RIDING").child(group_name).child("members").child("0").child("state").setValue("leader");
+                        ref.child("GROUP_RIDING").child(group_name).child("members").child("0").child("profile").setValue(userInfo[2]);
+
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+
+
+
+
+
+                int a = 1;
+                //TODO. lat, lon 자동완성 지우기
+                double lon = 127.0442899;
+                double lat = 37.6675547;
+
+                for (int i = 0; i < count+6 ; i=i+3)
+                {
+                    Log.e("print", array[i]);
+                    ref.child("GROUP_RIDING").child(group_name).child("members").child(String.valueOf(a)).child("uid").setValue(array[i+1]);
+                    ref.child("GROUP_RIDING").child(group_name).child("members").child(String.valueOf(a)).child("nickname").setValue(array[i]);
+                    ref.child("GROUP_RIDING").child(group_name).child("members").child(String.valueOf(a)).child("state").setValue("true");
+                    ref.child("GROUP_RIDING").child(group_name).child("members").child(String.valueOf(a)).child("profile").setValue(array[i+2]);
+                    //ref.child("GROUP_RIDING").child(group_name).child("members").child(String.valueOf(a)).child("lat").setValue(lat);
+                    //ref.child("GROUP_RIDING").child(group_name).child("members").child(String.valueOf(a)).child("lon").setValue(lon);
+                    lat = lat+0.1;
+                    lon = lon+0.1;
+                    a++;
+                }
+
                 Toast.makeText(CreateGroupActivity.this, "그룹이 생성되었습니다.", Toast.LENGTH_SHORT).show();
                 Intent intent=new Intent(CreateGroupActivity.this, GroupRideActivity.class);
                 startActivity(intent);
@@ -115,60 +195,6 @@ public class CreateGroupActivity extends AppCompatActivity {
     }
 
     public void UploadGroupInfoToFirebase() {
-        FirebaseUser mUser = FirebaseAuth.getInstance().getCurrentUser();
-        userRef = database.getReference("USER").child(mUser.getUid());
 
-        SharedPreferences getList = getSharedPreferences("com.el.ariby_preferences", MODE_PRIVATE);
-        int count = getList.getInt("count", 0);
-        String str = getList.getString("members", "none");
-        Log.d("members : ", str);
-        Log.d("count : ", String.valueOf(count));
-        final String group_name = groupName.getText().toString();
-        String start = inputStart.getText().toString();
-        String end = inputEnd.getText().toString();
-        String note = inputInfo.getText().toString();
-
-        String[] array = str.split("\\*");
-        //출발지
-        ref.child("GROUP_RIDING").child(group_name).child("startPoint").child("name").setValue(start);
-        ref.child("GROUP_RIDING").child(group_name).child("startPoint").child("lat").setValue(startY);
-        ref.child("GROUP_RIDING").child(group_name).child("startPoint").child("lon").setValue(startX);
-
-        //도착지
-        ref.child("GROUP_RIDING").child(group_name).child("endPoint").child("name").setValue(end);
-        ref.child("GROUP_RIDING").child(group_name).child("endPoint").child("lat").setValue(endY);
-        ref.child("GROUP_RIDING").child(group_name).child("endPoint").child("lon").setValue(endX);
-
-        //info
-        ref.child("GROUP_RIDING").child(group_name).child("info").child("note").setValue(note);
-
-        //생성자 닉네임
-        userRef.addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                ref.child("GROUP_RIDING").child(group_name).child("leader_nick").setValue(dataSnapshot.child("nickname").getValue());
-                ref.child("GROUP_RIDING").child(group_name).child("members").child("0").child("profile").setValue(dataSnapshot.child("userImageURL").getValue());
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
-            }
-        });
-        //멤버 정보 (리더, 멤버)
-        ref.child("GROUP_RIDING").child(group_name).child("members").child("0").child("uid").setValue(mUser.getUid());
-        ref.child("GROUP_RIDING").child(group_name).child("members").child("0").child("nickname").setValue("leader");
-        ref.child("GROUP_RIDING").child(group_name).child("members").child("0").child("state").setValue("true");
-
-        int a = 0;
-        for (int i = 0; i < count+6 ; i=i+3)
-        {
-            Log.e("print", array[i]);
-            ref.child("GROUP_RIDING").child(group_name).child("members").child(String.valueOf(a)).child("uid").setValue(array[i+1]);
-            ref.child("GROUP_RIDING").child(group_name).child("members").child(String.valueOf(a)).child("nickname").setValue(array[i]);
-            ref.child("GROUP_RIDING").child(group_name).child("members").child(String.valueOf(a)).child("state").setValue("true");
-            ref.child("GROUP_RIDING").child(group_name).child("members").child(String.valueOf(a)).child("profile").setValue(array[i+2]);
-            a++;
-        }
     }
 }
