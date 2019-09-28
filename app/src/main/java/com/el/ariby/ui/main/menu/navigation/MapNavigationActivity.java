8e68088cd4e601c68cf7111cfc23c5b037f13fb7
WG19 <mapofworld55@naver.com>
2019-09-29 00:42:39 +0900

    주행기록 업데이트

--- app/src/main/java/com/el/ariby/ui/main/menu/navigation/MapNavigationActivity.java
+++ app/src/main/java/com/el/ariby/ui/main/menu/navigation/MapNavigationActivity.java
@@ -65,6 +65,9 @@ public class MapNavigationActivity extends AppCompatActivity implements
     Timer timer = new Timer(); // 운행시간 체크
 
     ArrayList<String> disList = new ArrayList<>();
+    Date total = new Date(); //하루 누적 주행기록
+
+    String nickname, userProfile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -80,6 +83,7 @@ public class MapNavigationActivity extends AppCompatActivity implements
         //다운한 지도 데이터를 단말의 영구 캐쉬 영역에 저장하는 기능
         mapNaviView.setCurrentLocationTrackingMode(MapView.CurrentLocationTrackingMode.TrackingModeOff);
 
+
         Intent intent = getIntent();
 
         startY = intent.getStringExtra("startY");
@@ -87,7 +91,7 @@ public class MapNavigationActivity extends AppCompatActivity implements
         endY = intent.getStringExtra("endY");
         endX = intent.getStringExtra("endX");
         progressDialog = new ProgressDialog(MapNavigationActivity.this);
-        progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+        progressDialog.setMessage("데이터를 로딩중입니다.");
 
         disList.add(0, getMapFind(startY, startX, endY, endX)); // 총 거리 반환
         disList.add(1, startY);
@@ -124,9 +128,9 @@ public class MapNavigationActivity extends AppCompatActivity implements
                 CameraUpdateFactory.newMapPoint(markerPointStart, -1));
         mapNaviView.zoomIn(true);
 
+        progressDialog.show();
         try {
-            progressDialog.show();
-            Thread.sleep(3000);
+            Thread.sleep(2000);
             mapNaviView.setCurrentLocationTrackingMode(
                     MapView.CurrentLocationTrackingMode.TrackingModeOnWithHeading);
             mapNaviView.setCurrentLocationEventListener(this);
@@ -146,94 +150,7 @@ public class MapNavigationActivity extends AppCompatActivity implements
         timer.schedule(tt, 0, 1000);
     }
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-        timer.cancel();
 
-        userRef = database.getReference("USER").child(firebaseUser.getUid());
-        userRef.addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                String nickname = dataSnapshot.child("nickname").getValue().toString();
-                String userImg = dataSnapshot.child("userImageURL").getValue().toString();
-
-                Double myDistance = distance(
-                        Double.valueOf(disList.get(3)), //start Y
-                        Double.valueOf(disList.get(4)), //start X
-                        Double.valueOf(disList.get(1)), //현재위치 Y
-                        Double.valueOf(disList.get(2)), "kilometer"); // 현재위치 X
-
-                Double dis = Math.round(myDistance * 10) / 10.0;
-                if (!TextUtils.isEmpty(myDistance.toString())) {
-                    if (myDistance > 0.0) {
-                        //현재 날짜/시간 가져오기
-                        long currentTime = System.currentTimeMillis();
-                        final Date date = new Date(currentTime);
-                        Date month = new Date(currentTime);
-                        SimpleDateFormat sdfYear = new SimpleDateFormat("yyyy");
-                        SimpleDateFormat sdfMonth = new SimpleDateFormat("MM");
-                        SimpleDateFormat todayFormat = new SimpleDateFormat("yyyy-MM-dd");
-                        //나중에 사용
-                        String today = todayFormat.format(date);
-                        String thisYear = sdfYear.format(date);
-                        String thisMonth = sdfMonth.format(date);
-                        String monthCheckStr = thisMonth + "-01-" + thisYear + " 00:00:00";
-
-                        DateFormat format = new SimpleDateFormat("MM-dd-yyyy HH:mm:ss");
-                        try {
-                            month = format.parse(monthCheckStr);
-                        } catch (ParseException e) {
-                            e.printStackTrace();
-                        }
-
-                        long monthOutput = month.getTime() / 1000L;
-                        String monthStr = Long.toString(monthOutput);
-                        final long monthTimestamp = Long.parseLong(monthStr) * 1000;
-
-                        Log.e("스탬프", String.valueOf(monthTimestamp));
-
-                        SimpleDateFormat format2 = new SimpleDateFormat("hh:mm:ss");
-                        String totalTime = format2.format(new Date(time));
-
-                        Log.e("테스트:걸린시간 = ", totalTime);
-
-                        ref.child(firebaseUser.getUid())
-                                .child("userInfo")
-                                .setValue(new MemberListItem(userImg, nickname));
-
-                        ref.child(firebaseUser.getUid())
-                                .child("dailyData")
-                                .child(String.valueOf(monthTimestamp))
-                                .child("dailyTotalTime")
-                                .setValue(getTime(time));
-
-                        ref.child(firebaseUser.getUid())
-                                .child("dailyData")
-                                .child(String.valueOf(monthTimestamp))
-                                .child("dailyTotalDis")
-                                .setValue(dis);
-
-                        ref.child(firebaseUser.getUid())
-                                .child("dailyData")
-                                .child(String.valueOf(monthTimestamp))
-                                .child("date")
-                                .setValue(today);
-
-                        Log.e("테스트", String.valueOf(myDistance));
-                    }
-                }
-            }
-
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
-            }
-        });
-
-
-    }
 
     private String getMapFind(final String startX, final String startY,
                               final String endX, final String endY) {
@@ -397,6 +314,165 @@ public class MapNavigationActivity extends AppCompatActivity implements
         Log.d("currentDistance", String.valueOf(distanceKiloMeter));
         Log.d("테스트 : LAT", String.valueOf(mapPointGeo.latitude));
         Log.d("테스트 : LONG", String.valueOf(mapPointGeo.longitude));
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        timer.cancel();
+
+        Double myDistance = distance(
+                Double.valueOf(disList.get(3)), //start Y
+                Double.valueOf(disList.get(4)), //start X
+                Double.valueOf(disList.get(1)), //현재위치 Y
+                Double.valueOf(disList.get(2)), "kilometer"); // 현재위치 X
+
+        final Double dis = Math.round(myDistance*10)/10.0;
+        final FirebaseUser mUser = FirebaseAuth.getInstance().getCurrentUser();
+        final String myUid = mUser.getUid();
+        Log.e("myUid : ", myUid);
+        long currentTime = System.currentTimeMillis();
+        final Date date = new Date(currentTime);
+        Date rightNow = new Date(currentTime); //단일 주행기록
+
+        SimpleDateFormat sdfYear = new SimpleDateFormat("yyyy");
+        SimpleDateFormat sdfMonth = new SimpleDateFormat("MM");
+        SimpleDateFormat sdfDay = new SimpleDateFormat("dd");
+        SimpleDateFormat sdfHour = new SimpleDateFormat("HH");
+        SimpleDateFormat sdfMin = new SimpleDateFormat("mm");
+        SimpleDateFormat sdfSec = new SimpleDateFormat("ss");
+        SimpleDateFormat todayFormat = new SimpleDateFormat("yyyy-MM-dd");
+
+        //나중에 사용
+        final String today = todayFormat.format(date);
+        String thisYear = sdfYear.format(date);
+        String thisMonth = sdfMonth.format(date);
+        String thisDay = sdfDay.format(date);
+        String thisHour = sdfHour.format(date);
+        String thisMin = sdfMin.format(date);
+        String thisSec = sdfSec.format(date);
+        final String now = thisYear + thisMonth + thisDay + thisHour;
+        String rightNowStr = thisMonth + "-" + thisDay + "-" + thisYear + " " + thisHour + ":" + thisMin + ":" + thisSec; //단일 주행기록
+        String dailyTotalStr = thisMonth + "-" + thisDay + "-" + thisYear + " 09:00:00"; //누적 주행기록
+        Log.e("right now : ", rightNowStr);
+        Log.e("total : ", dailyTotalStr);
+        DateFormat format = new SimpleDateFormat("MM-dd-yyyy HH:mm:ss");
+        try {
+            rightNow = (Date) format.parse(rightNowStr); //단일
+            total = (Date)format.parse(dailyTotalStr); //누적
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        long output = rightNow.getTime() / 1000L; //하루 단일 주행기록
+        String str = Long.toString(output);
+        final long nowTimestamp = Long.parseLong(str) * 1000;
+
+        long totalOutPut = total.getTime() / 1000L; //누적 주행기록
+        String totalStr = Long.toString(totalOutPut);
+        final long totalTimestamp = Long.parseLong(totalStr) * 1000;
+
+        Log.e("rightNow? : ", String.valueOf(nowTimestamp));
+        Log.e("누적 주행기록 : ", String.valueOf(totalTimestamp));
+        final String route = "dailyData/data/" + nowTimestamp + "/";
+
+        final boolean[] uploadCheck = {false};
+        if(myDistance > 0.0){
+            userRef = database.getReference("USER");
+
+            userRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                    nickname = dataSnapshot.child(myUid).child("nickname").getValue().toString();
+                    userProfile = dataSnapshot.child(myUid).child("userImageURL").getValue().toString();
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                }
+            });
+
+            ref.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    for(DataSnapshot dataSnapshot1 : dataSnapshot.getChildren() ){
+                        String uid = dataSnapshot1.getKey();
+                        Log.d("uid : ", uid);
+                        if(myUid.equals(uid)){ //해당 uid로 기존 기록이 있으면
+                            try { //누적기록 업데이트
+                                String preDis = dataSnapshot1.child("dailyData").child(String.valueOf(totalTimestamp)).child("dailyTotalDis").getValue().toString();
+                                String preTime = dataSnapshot1.child("dailyData").child(String.valueOf(totalTimestamp)).child("dailyTotalTime").getValue().toString();
+                                Log.e("preDis : ", preDis);
+                                if(preDis!=null && preTime!=null){
+                                    Double updateDis = Double.parseDouble(preDis) + dis;
+
+                                    String [] times = preTime.split(":");
+                                    String [] inputTime = getTime(time).split(":");
+
+                                    int [] timeInt = new int[times.length];
+                                    int [] inputTimeInt = new int[inputTime.length];
+                                    for(int i=0; i<times.length ; i++){
+                                        timeInt[i] = Integer.parseInt(times[i]);
+                                        inputTimeInt[i] = Integer.parseInt(inputTime[i]);
+                                        timeInt[i] += inputTimeInt[i];
+                                    }
+
+                                    for(int z=2;z >=0;z--){
+                                        if(timeInt[z]>=60){
+                                            timeInt[z-1] += timeInt[z]/60;
+                                            timeInt[z] = timeInt[z]%60;
+                                        }
+                                    }
+                                    Log.d("timeInt", String.valueOf(timeInt[0])+"시, "+timeInt[1]+", "+timeInt[2]);
+                                    String updateTime = timeInt[0]+":"+timeInt[1]+":"+timeInt[2];
+
+                                    ref.child(myUid).child("dailyData").child(String.valueOf(totalTimestamp)).child("dailyTotalDis").setValue(updateDis);
+                                    ref.child(myUid).child("dailyData").child(String.valueOf(totalTimestamp)).child("dailyTotalTime").setValue(updateTime);
+
+                                }else{ //당일 누적기록이 없으면
+                                    ref.child(myUid).child("dailyData").child(String.valueOf(totalTimestamp)).child("dailyTotalTime").setValue(getTime(time));
+                                    ref.child(myUid).child("dailyData").child(String.valueOf(totalTimestamp)).child("dailyTotalDis").setValue(dis);
+                                    ref.child(myUid).child("dailyData").child(String.valueOf(totalTimestamp)).child("date").setValue(today);
+                                }
+
+                            }catch (Exception e){
+                                e.printStackTrace();
+                            }
+
+                            uploadCheck[0] = true;
+                            break;
+                        }else{ //해당 uid로 기존 기록이 없으면 userInfo도 업로드
+
+                            //누적기록
+                            ref.child(myUid).child("dailyData").child(String.valueOf(totalTimestamp)).child("dailyTotalTime").setValue(getTime(time));
+                            ref.child(myUid).child("dailyData").child(String.valueOf(totalTimestamp)).child("dailyTotalDis").setValue(dis);
+                            ref.child(myUid).child("dailyData").child(String.valueOf(totalTimestamp)).child("date").setValue(today);
+
+                            //유저 인포
+                            ref.child(myUid).child("userInfo").child("nickname").setValue(nickname);
+                            ref.child(myUid).child("userInfo").child("profile").setValue(userProfile);
+                            break;
+                        }
+
+                    }
+                    //단일기록 저장
+                    ref.child(myUid).child("dailyData").child("data").child(String.valueOf(nowTimestamp)).child("distance").setValue(dis);
+                    ref.child(myUid).child("dailyData").child("data").child(String.valueOf(nowTimestamp)).child("time").setValue(getTime(time));
+                    ref.child(myUid).child("dailyData").child("data").child(String.valueOf(nowTimestamp)).child("date").setValue(today);
+
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                }
+
+            });
+        }else{
+            Toast.makeText(this, "주행기록이 0km 입니다.", Toast.LENGTH_SHORT).show();
+        }
+
     }
 
     private static double distance(double lat1, double lon1, double lat2, double lon2, String unit) {
