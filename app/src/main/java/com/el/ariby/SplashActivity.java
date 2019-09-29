4ab48244505e6e4f105bbc122941a44b804415a3
takedawon <takedawon@naver.com>
2019-09-29 21:37:35 +0900

    스플래시 더 빠르게 올라오도록 수정

--- app/src/main/java/com/el/ariby/SplashActivity.java
+++ app/src/main/java/com/el/ariby/SplashActivity.java
@@ -48,7 +48,7 @@ public class SplashActivity extends AppCompatActivity {
                 try {
                     int waited = 0;
                     // Splash screen pause time
-                    while (waited < 3500) { // 켜지는 시간 (약 3.5초)
+                    while (waited < 2000) { // 켜지는 시간 (약 3.5초)
                         sleep(100);
                         waited += 100;
                     }
