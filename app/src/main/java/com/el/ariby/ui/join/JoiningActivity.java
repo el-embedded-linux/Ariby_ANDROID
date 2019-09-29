c8ee77a115b4d3214960f00847f64168744600d6
takedawon <takedawon@naver.com>
2019-09-29 19:40:01 +0900

    회원가입시 백버튼 클릭시 회원가입창으로 가지던 버그 수정

--- app/src/main/java/com/el/ariby/ui/join/JoiningActivity.java
+++ app/src/main/java/com/el/ariby/ui/join/JoiningActivity.java
@@ -119,6 +119,7 @@ public class JoiningActivity extends AppCompatActivity {
                                                             if (task.isSuccessful()) {
                                                                 Intent intent = new Intent(JoiningActivity.this, MainActivity.class);
                                                                 progressDialog.dismiss();
+                                                                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
                                                                 startActivity(intent);
                                                             } else {
                                                                 Toast.makeText(JoiningActivity.this, "아이디,비밀번호가 틀리거나 없는 아이디입니다.", Toast.LENGTH_SHORT).show();
